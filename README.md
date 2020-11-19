dbb85f0c0bfdcc7b283490b8265da207c83e96f2
KLacerda08 <kaldmr7880@gmail.com>
2020-11-18 23:07:47 -0500

    cleaned up code, added readme

--- README.md
+++ README.md
@@ -1,1 +1,85 @@
-# sqlalchemy-challenge
+# Surf's Up
+
+![Title](Images/tubewave.jpg)
+
+## Table of contents
+* [Assignment](#assignment)
+* [Data Sources](#data_sources)
+* [Evaluation](#eval)
+* [Conclusions](#conclusions)
+* [References](#ref)
+
+## Assignment
+Research temperature data in Hawaii to aid in vacation planning.  
+
+## Data Sources and Assumptions
+The data was housed in the following sqlite database: hawaii.sqlite
+
+Two (2) csv files were also provided as backup references: 
+- hawaii_measurements.csv
+- hawaii_stations.csv
+
+Link to data sources: https://github.com/KLacerda08/sqlalchemy-challenge/tree/main/Resources 
+
+## Evaluation
+A connection was made to the sqlite database using python in jupyter notebook. The tables within the database were inspected to identify the 
+columns in each. Queries were designed to find the following data:  
+
+1. Find last 12 months of precipitation data in the database and visualize the precipitation over time. Prior to plotting, it was necessary to 
+	average the precipitation for each day, since multiple measurements were made per day.  The following chart shows the preciptation 
+	over time: 
+
+![Title](Images/daily_precip_time.jpg)
+
+	The results show that the most consistent spans of heavy rain generally occured between February and early March, mid April to early May. 
+	However, the highest precipitation occured in September, suggesting a weather event such as a hurricane.    
+
+2. To evaluate precipitation data on a monthly scale, the average precipitation per month was also calculated and visualized:  
+
+![Title](Images/monthly_precip.jpg)
+
+	The monthly results suggest that late August to September have the most average rain. However, these monthly averages may be influenced 
+	by weather events, such as hurricanes.  Using 0.15 inches per day as an average upper limits, if  a vacation wanted to avoid the rain, 
+	the best times to visit Hawaii would be from May to early August, October to November, or in January.  
+ 
+3. Precipitation statistics were also calculated for the most recent year of data.  The average precipitation was 0.17 inches per day, with 
+	with a maximum precipitation of 2.38 inches.   
+
+4. The total number of weather stations was identified (9 stations), and the number of measurements at each station was calculated. The most
+	frequently measured station was USC00519281, with 2,772 measurements. 
+
+5. Additional statistics were calculated for the most frequently measured station across the entire dataset (USC00519281): 
+		Mininum Temperature: 54.0 (deg F)
+		Maximum Temperature: 85.0
+		Average Temperature: 71.66
+
+6. The most recent 12 months of temperature data from the top measured station (USC00519281) was evaluated and visualized.  
+
+![Title](Images/hist_temps.jpg)
+
+	Based on these data, the climate of Hawaii is generally mild, with most temperature observations falling between approximately 67.5 
+	and 81 degrees Fahrenheit.  
+
+7. A weather app was designed to allow the user to query temperature and precipitation data as follows:  
+
+	1.  The most recent 12 months of precipitation data.  
+	2.  The list of measurement stations.
+	3.  The station ID of the most active station during the latest year of data (USC00519397).  
+	4.  The temperature observations from the most recent year at the station that was most actively monitored during the most recent year. 
+	5.  The minimum, maximum, and average temperature observations from a user input start date until the most recent date of observations
+	    in the data (results vary with user input).  
+	6.  The minimum, maximum, and average temperature observations from a user input date range (results vary with user input).   
+	7.  An about page was also included to provide the user information on the data background 
+
+## Conclusions
+
+* Based on the daily precipitation data, the most consistent spans of heavy rain generally occured between February and early March, mid April
+	to early May. 	However, the highest precipitation occured in September, suggesting a severe weather event such as a hurricane.
+* The monthly average precipitation data suggest that late August to September have the most average rain. However, these monthly averages may 
+	be influenced by severe weather events, such as hurricanes.  Using 0.15 inches per day as an average upper limit, if a vacationer 
+	wanted to avoid rainy weather, the best times to visit Hawaii would be from May to early August, October to November, or in January.   
+* Based on the temperature data, the climate of Hawaii is generally mild, with most temperature observations falling between approximately 
+	67.5 nd 81 degrees Fahrenheit.  
+
+## References
+1. image source: https://www.surfertoday.com/surfing/what-is-the-hawaiian-wave-scale
--- app.py
+++ app.py
@@ -47,20 +47,22 @@ def home():
         f"<h1>Welcome to our Weather Data Homepage!</h1>"
         f"<h2>Available Routes:</h2>"
         f"About Us: <br/>/about<br/><br/>"
-        f"Precipitation Data:  <br/>/api/v1.0/precipitation<br/><br/>"
+        f"Precipitation Data - Most Recent 12 Months:  <br/>/api/v1.0/precipitation<br/><br/>"
         f"Station List:  <br/>/api/v1.0/stations<br/><br/>"
-        f"Top Measured Station ID:  <br/>/api/v1.0/id_top_station<br/><br/>"
-        f"Temperature Data from Top Station: <br/>/api/v1.0/tobs_top_station<br/><br/>"
+        f"Most Frequently Measured Station (in Last 12 Months): <br/>/api/v1.0/id_top_station<br/><br/>"
+        f"Temperature Data from Most Frequently Measured Station: <br/>/api/v1.0/tobs_top_station<br/><br/>"
         f"Temperature Stats by Start Date (YYYY-MM-DD): <br/>/api/v1.0/start<br/><br/>"
         f"Temperature Stats by Date Range (YYYY-MM-DD): <br/>/api/v1.0/start/end")
 
 @app.route("/about")
 def about(): 
     return (
-            f"Welcome!  We provide weather data for Hawaii! <br/>"
-            f"To search temperature data by date, use date format: YYYY-MM-DD.<br/>"
-            f"The most frequently measured station ID can be found at the following route:\
-                /api/v1.0/id_top_station<br/>")
+            f"<h2>About Us:  We provide weather data for Hawaii!</h2>"
+            f" The database includes temperature and precipitation monitoring of several weather stations \
+                throughout Hawaii from January 1, 2010 through August 23, 2017.<br/>\
+                To search temperature data by date, use date format: YYYY-MM-DD.<br/>\
+                The most frequently measured station ID can be found at the following route:\
+                /api/v1.0/id_top_station")
 
 @app.route('/api/v1.0/precipitation')
 def precipitation():
@@ -75,7 +77,7 @@ def precipitation():
         filter(measurement.date > start_date).\
         group_by(measurement.date).all() 
 
-    # Save the query results as a Pandas DataFrame, set the index to the date column, round precip values
+    # Save the query results as a DataFrame, set the index to the date column, round precip values
     prcp_df = pd.DataFrame(last_year, columns = ['Date', 'Precip (in)'])
     prcp_df.set_index('Date',inplace=True)
     prcp_df['Precip (in)'] = prcp_df['Precip (in)'].round(2)
@@ -97,43 +99,39 @@ def stations():
     session.close()
     return jsonify(sta_list)
 
-
 @app.route('/api/v1.0/id_top_station')
 def id_top_station():
-    # Added this page to list the "top station" so that the tobs page can make sense to the user.  
+    # Added this page to identify the "top station" (most active station) for the latest year of data.  
     session = Session(engine)
-    
+   
+    # Get start and end dates of most recent 12 months of data  
+    latest_date = session.query(measurement.date).order_by(measurement.date.desc()).first()
+    start_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
     # Query the dates and temperature observations of the most active station for the latest year of data.
     top_sta = session.query(measurement.station).\
         group_by(measurement.station).\
+        filter(measurement.date > start_date).\
         order_by(func.count(measurement.station).desc()).first()
-
-    latest_date2 = session.query(measurement.date).\
-        filter(measurement.station == 'USC00519281').\
-        order_by(measurement.date.desc()).first()
-
-    start_date2 = dt.date(2017, 8, 18) - dt.timedelta(days=365)
 
     session.close()
     return (
-        f'<h3>The most frequently measured weather station for the most recent year of data ({start_date2} to \
-            {latest_date2}) was:</h3> &nbsp &nbsp {top_sta}')
+        f'<h3>The most frequently measured weather station for the most recent year of data ({start_date} to \
+            {latest_date}) was:</h3> &nbsp &nbsp {top_sta}'
+            )
 
 @app.route('/api/v1.0/tobs_top_station')
 def tobs():
     session = Session(engine) 
 
-    #Redefine dates for this session query
-    latest_date2 = session.query(measurement.date).\
-        filter(measurement.station == 'USC00519281').\
-        order_by(measurement.date.desc()).first()
+    # Get start and end dates of most recent 12 months of data  
+    latest_date = session.query(measurement.date).order_by(measurement.date.desc()).first()
+    start_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
 
-    start_date2 = dt.date(2017, 8, 18) - dt.timedelta(days=365)
-    #Get tobs from the station identified in the prior page(USC00519281)
-
+    #Get tobs from the station identified in the prior page(USC00519397)
     tobs2 = session.query(measurement.date, measurement.tobs).\
-        filter(measurement.station == 'USC00519281').\
-        filter(measurement.date > start_date2).all()
+        filter(measurement.station == 'USC00519397').\
+        filter(measurement.date > start_date).all()
     
     # Convert to dictionary
     tobs2_df = pd.DataFrame(tobs2, columns = ["Date", 'tobs'])
@@ -143,7 +141,6 @@ def tobs():
     
     session.close()
     return jsonify(tobs2_dict)
-
 
 @app.route('/api/v1.0/<start>')
 def search_by_start_date(start):
